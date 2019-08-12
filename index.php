076abd9ec75eadf87c1ebe4df4d4da04293c6a5d
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-12 23:32:43 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head><title>HOME PAGE</title>
+<body>
+<center><h1>GuillemerJ Photo Gallery</h1>
+<3>LIST OF PHOTOS</3>
+<a href="photo1.php">PHOTO 1 ME ONLY</a><br>
+<a href="photo2.php">PHOTO 2 ME AND MY GIRLFRIEND</a><br>
+<a href="photo3.php">PHOTO 3 ME AND MY FAMILY</a><br>
+</body>
+</html>
+
+
