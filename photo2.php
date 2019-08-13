2eea897dfa7f7001cc8a1daf9418a40f1b8c1dbe
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-13 10:31:14 +0800

    Create photo2.php

---
+++ photo2.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHOTOS OF JECHRISTOFFER</title>
+<body>
+<center><h1>GuillemerJ Photo Gallery</h1>
+<img src="jay2.jpg"></center>
+<a href="index.php"<b>BACK TO HOMEPAGE</b></a>
+<a href="photo3.php" style = "float": right;"><b>NEXT PHOTO</b></a>
+</body>
