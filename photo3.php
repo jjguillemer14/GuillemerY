268f524b689545d671df892dfe51d6dd6f735573
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-13 10:35:27 +0800

    Create photo3.php

---
+++ photo3.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHOTOS OF JECHRISTOFFER</title>
+<body>
+<center><h1>GuillemerJ Photo Gallery</h1>
+<img src="jay3.jpg"></center>
+<a href="index.php"<b>BACK TO HOMEPAGE</b></a>
+</body>
