16bcaf8aafafadb590e9c5168c9c89be6f9ca07a
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-14 09:49:06 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -4,7 +4,7 @@
 <body>
 <center><h1>GuillemerJ Photo Gallery</h1>
 <img src="Jay1.jpg"></center>
-<a href="index.php"<b>BACK TO HOMEPAGE</b></a>
+<a href="index2.php"<b>BACK TO HOMEPAGE</b></a>
 <a href="photo2.php" style = "float": right;"><b>NEXT PHOTO</b></a>
 </body>
 </html>
