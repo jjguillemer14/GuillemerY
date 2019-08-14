a1c4690c0998008d24876fd83b2885717104d681
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-14 10:03:23 +0800

    Create Sign up.php

---
+++ Sign up.php
@@ -0,0 +1,26 @@
+<!DOCTYP html>
+<html>
+<head><title>Reset Password</title>
+
+<body align ="center">
+<h2>Sign Up</h3>
+</align>
+<center>
+<h4>Email Address:<input type"text></h4>
+<h4>Confirm Email:<input type"text></h4>
+<h4>Password:<input type"text></h4>
+<h4>Confirm Password:<input type"text></h4>
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<h4> Please enter you Email Address:
+<input type= "text" placeholder=" Input your Email Addres."</h4>
+<input type = "submit" value ="Sign Up">
+
+</body>
+</html>
