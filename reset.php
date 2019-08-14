a6113b9ed0a6c3be860363a176e2a569ef0f8246
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-14 14:56:36 +0800

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><title>Reset Password</title>
+</head>
+<body align="center bgcolor="brown">
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<form action="resetpassword2.php">
+<h4>Enter an Email Address:
+<input type="placeholder=" Input your Email Address."</h4>
+<input type="submit" value"Reset password>
+</form>
+</body>
+</html>
