f2ec815190a0722aa49fa788074d23c04a97700a
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-14 10:04:54 +0800

    Create Reset.php

---
+++ Reset.php
@@ -0,0 +1,18 @@
+<!DOCTYP html>
+<html>
+<head><title>LOG.IN</title>
+
+<body align ="center">
+<h1>Login</h1>
+<br/>
+<form submit ="/submit_page.php">
+<h4>Username: <input type "user"></h4>
+<h4>Password:<input type"password></h4>
+<input type ="submit" value="Sign in">
+<br/>
+<a href="resetpassword.php">Reset Password</a>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="sign up.php">Sign Up</a>
+</form>
+</body>
+</html>
