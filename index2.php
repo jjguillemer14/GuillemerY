4724abe8dc096131ed507c3718cc464705778d13
jjguillemer14 <53499675+jjguillemer14@users.noreply.github.com>
2019-08-14 14:44:13 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -3,7 +3,7 @@
 <head><title>HOME PAGE</title>
 <body>
 <center><h1>GuillemerJ Photo Gallery</h1>
-<3>LIST OF PHOTOS</3>
+<h3>LIST OF PHOTOS</h3>
 <a href="photo1.php">PHOTO 1 ME ONLY</a><br>
 <a href="photo2.php">PHOTO 2 ME AND MY GIRLFRIEND</a><br>
 <a href="photo3.php">PHOTO 3 ME AND MY FAMILY</a><br>
